dbc86bfeb61590b3fd46ffdb433fef4e64671728
Ref-Bit <rsv.gho@gmail.com>
2020-07-11 10:39:59 +0300

    Updated assets for Heroku upload

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href={{ asset('css/app.css') }}>
+        <link rel="stylesheet" href={{ asset('css/app.css', true) }}>
         <title>BookVel</title>
     </head>
     <body>
@@ -368,6 +368,6 @@
           </div>
         </div>
       </div>
-      <script src={{ asset('js/app.js') }}></script>
+      <script src={{ asset('js/app.js', true) }}></script>
     </body>
 </html>
