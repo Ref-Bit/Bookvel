75e0c9dabb2cdd3af2cfb33dd0c0585e4a841b31
Ref-Bit <rsv.gho@gmail.com>
2020-07-11 10:56:56 +0300

    Updated assets for environment change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href={{ asset('css/app.css', true) }}>
+        <link rel="stylesheet" href={{ env('APP_ENV') === 'local' ? asset('css/app.css') : asset('css/app.css', true) }}>
         <title>BookVel</title>
     </head>
     <body>
       <div id="app"></div>
-      <script src={{ asset('js/app.js', true) }}></script>
+      <script src={{ env('APP_ENV') === 'local' ? asset('js/app.js') : asset('js/app.js', true) }}></script>
     </body>
 </html>
